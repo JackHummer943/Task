cd6d91bbe0534c9a599d6a12e9153c00fa7d3bc4
devJ <bessonov9751@gmail.com>
2024-02-21 11:04:02 +0300

    Task2_5 update2

--- src/Task2/Task2_5.java
+++ src/Task2/Task2_5.java
@@ -7,16 +7,19 @@ import static java.lang.System.in;
 
 public class Task2_5 {
     public static void main(String[] args) {
-        Scanner id = new Scanner(in);
-        long m = (long)id.nextDouble();
-        String n = Long.toString(m);
-        int k = n.length();
-        if(m < 1000L) {
-            System.out.println("Число меньше 1 000");
-        } else {
-            PrintStream value10000 = System.out;
-            char value10001 = n.charAt(k - 4);
-            value10000.println("Число тысяч - " + value10001);
-        }
+       Scanner id = new Scanner(System.in);
+       int n = id.nextInt();
+       String m = Integer.toString(n);
+       int k = m.length();
+       if(n < 1000) {
+           System.out.println("Число меньше 1 000, число тысяч - 0");
+       } else if (n > 10000){
+           System.out.println("Число не входит в диапазон от 1000 до 10000");
+       } else {
+           PrintStream value10000 = System.out;
+           char value10001 = m.charAt(k - 4);
+           value10000.println("Число тысяч - " + value10001);
+       }
+
     }
 }
