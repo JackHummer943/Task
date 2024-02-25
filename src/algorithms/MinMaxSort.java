f42e2e494d25160ee961df4dbc38f5c20fe12942
devJ <bessonov9751@gmail.com>
2024-02-25 23:28:39 +0300

    MinMaxSort version3

--- src/algorithms/MinMaxSort.java
+++ src/algorithms/MinMaxSort.java
@@ -1,46 +1,40 @@
-package algorithms;
 import java.util.Arrays;
 
 public class MinMaxSort {
     public static void main(String[] args) {
-        int [] arr = {5, 3, 2, 1, 7};
-        System.out.println("Неотсортированный массив: " + Arrays.toString(arr) );
-        minmax(arr);
-        System.out.println("Отсортированный массив: " + Arrays.toString(arr));
+        int [] arr = {4,8,0,1,2,5,7};
+        MinMaxSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void MinMaxSort(int[] arr){
+        int start, end, step, min, max, min_index, max_index, temp;
+        for (start = 0, end = arr.length - 1; start < end; start++, end--) {
+            min = max = arr[start];
+            min_index = max_index = start;
+            for (step=start; step <= end; step++) {
+                if (arr[step] > max) {
+                    max = arr[step];
+                    max_index = step;
+                }
+                else if (arr[step] < min){
+                    min = arr[step];
+                    min_index = step;
+                }
+            }
+            temp = arr[start];
+            arr[start] = arr[min_index];
+            arr[min_index] = temp;
+            if (arr[min_index] == max) {
+                temp = arr[end];
+                arr[end] = arr[min_index];
+                arr[min_index] = temp;
+            }
+            else {
+                temp = arr[end];
+                arr[end] = arr[max_index];
+                arr[max_index] = temp;
+            }
+        }
     }
 
-   public static void minmax (int[] arr)     {
-       int i, j, k, min, max, min_indx, max_indx, tmp;
-       for (i=0, j=arr.length-1; i<j; i++, j--)
-       {
-
-           min=max=arr[i];
-
-           min_indx = max_indx = i;
-
-           for (k=i;k<=j;k++)
-           {
-               if (arr[k] > max) {
-                   max = arr[k];
-                   max_indx = k;
-               } else if (arr[k] < min) {
-                   min = arr[k];
-                   min_indx = k;
-               }
-           }
-           tmp = arr[i];
-           arr[i] = arr[min_indx];
-           arr[min_indx] = tmp;
-           if(arr[min_indx] == max){
-               tmp = arr[j];
-               arr[j] = arr[min_indx];
-               arr[min_indx]=tmp;
-           }
-           else {
-               tmp=arr[j];
-               arr[j]=arr[max_indx];
-               arr[max_indx] = tmp;
-           }
-       }
-   }
 }
