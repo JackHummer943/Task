627f339099e412de328e528af7de54051e0b534d
devJ <bessonov9751@gmail.com>
2024-02-28 12:37:51 +0300

    task complete v.2

--- src/test/Reverse.java
+++ src/test/Reverse.java
@@ -3,23 +3,13 @@ package test;
 import java.util.List;
 
 public class Reverse {
-   public List reverse(List list) {
-       // пустой лист - пустой список
-       // не пустой список - список
-       // в качестве терминирующего значения - пустой список
-       // [1,2,3] => 1 [2,3]
-       // [2,3]++1 => [2,3,1]
-       // [2,3] => [3,2]
-       // [3,2]++1 -> [3,2,1]
-       // Псевдокод
-       // reverse[] -> []
-       // reverse[head + tail] -> reverse [tail]++ head
-    if (list.size() == 0){
+    public List reverse(List list) {
+        if (list.size() == 0) {
+            return list;
+        }
+
+        list.add(0, list.remove(list.size() - 1));
+        reverse(list.subList(1, list.size()));
         return list;
     }
-
-       reverse(list.subList(1, list.size())).add(list.get(0));
-        list.remove(0);
-       return list;
-   }
 }
