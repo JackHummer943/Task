e1472edbff7dfc5da3dfff33ecff659c3f80fe75
devJ <bessonov9751@gmail.com>
2024-03-01 11:14:22 +0300

    MinMaxSort 01.03(v7)

--- src/algorithms/MinMaxSort.java
+++ src/algorithms/MinMaxSort.java
@@ -1,39 +1,40 @@
 package algorithms;
 
 import java.util.Arrays;
+
 public class MinMaxSort {
     public static void main(String[] args) {
-        int arr[] = {9,9,3,2,1,4,0,-1,11,99,5};
-        MinMaxSort(arr);
-        System.out.println(Arrays.toString(arr));
+        int []mas = {2,9,3,7,4,0,-1,1};
+        MinMax(mas);
+        System.out.println(Arrays.toString(mas));
     }
-    public static void MinMaxSort (int[] arr){
-        int start, end, step, maxVal, minVal, min_index, max_index, timeVal;
-        for (start=0, end = (arr.length-1); start < end; start++, end--) {
-             minVal = maxVal = arr[start];
-             min_index = max_index = start;
-for (step = start; step <= end; step++) {
-
-    if (arr[step] > maxVal) {
-        maxVal = arr[step];
-        max_index = step;
-    } else if ( minVal > arr[step] ) {
-        minVal = arr[step];
-        min_index = step;
+    public static void MinMax(int[] mas) {
+        int start, end, step, maxVal, minVal, minIndex, maxIndex, timeVal;
+        for (start = 0, end = mas.length - 1; end > start; start++, end--) {
+            maxVal = minVal = mas[start];
+            minIndex = maxIndex = start;
+            for (step = start; step<=end; step++) {
+                if (mas[step] > maxVal) {
+                    maxVal = mas[step];
+                    maxIndex = step;
+                } else if (mas[step] < minVal) {
+                    minVal = mas[step];
+                    minIndex = step;
+                }
+            }
+            timeVal = mas[start];
+            mas[start] = mas[minIndex];
+            mas[minIndex] = timeVal;
+            if(mas[minIndex] == maxVal) {
+                timeVal = mas[end];
+                mas[end] = mas[minIndex];
+                mas[minIndex] = timeVal;
+            }
+            else {
+                timeVal = mas[end];
+                mas[end] = mas[maxIndex];
+                mas[maxIndex] = timeVal;
+            }
+        }
     }
-        }
-        timeVal = arr[start];
-        arr[start] = arr[min_index];
-        arr[min_index] = timeVal;
-        if(arr[min_index] == maxVal) {
-            timeVal = arr[end];
-            arr[end] = arr[min_index];
-            arr[min_index] = timeVal;
-        }
-        else {
-            timeVal = arr[end];
-            arr[end] = arr[max_index];
-            arr[max_index] = timeVal;
-        }
-    }
-}}
+}
